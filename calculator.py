fc533f1bf88650d2483b58d6275ecf82e0f2614c
Yuri Costa <yuricosta2319@gmail.com>
2024-04-12 20:40:04 -0300

    Adding tests for adition and subtraction functions

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
-def soma(x, y):
-    assert isinstance(x, (int, float)), 'X precisa ser int ou float'
-    assert isinstance(x, (int,float)), 'Y precisa ser int ou float'
+def sum(x, y):
+    assert isinstance(x, (int, float)), 'X need to be int or float'
+    assert isinstance(x, (int,float)), 'Y need to be int or float'
     return x + y
 
+def subtract(x, y):
+    assert isinstance(x, (int, float)), 'X need to be int or float'
+    assert isinstance(y, (int,float)), 'Y need to be int or float'
+    return x - y
---
+++ teste_calculator.py
@@ -0,0 +1,38 @@
+import unittest
+from calculator import sum,subtract
+
+class TestCalculadora(unittest.TestCase):
+    def test_sum_5_5_must_return_10(self):
+        self.assertEqual(sum(5,5), 10)
+
+    def test_sum_5_negative_and_5_must_return_0(self):
+        self.assertEqual(sum(-5,5), 0)
+
+    def test_sum_multiple_entries(self):
+        x_y_exits=(
+        (10, 10, 20),
+        (5, 5, 10),
+        (1.5, 1.5, 3.0),
+        (-5, 5, 0),
+        (100, 100, 200),
+        )
+
+        for x_y_exit in x_y_exits:
+            with self.subTest(x_y_saida=x_y_exits):
+                x, y, exit = x_y_exit
+                self.assertEqual(sum(x,y), exit) 
+
+    def test_x_not_is_int_or_float_must_return_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            sum("10", 0)
+
+    def test_y_not_is_int_or_float_must_return_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            sum(0, "10")
+
+    def test_subtraction_5_4_must_return_1(self):
+        self.assertEqual(subtract(5,4), 1)
+
+
+
+unittest.main(verbosity=2)
